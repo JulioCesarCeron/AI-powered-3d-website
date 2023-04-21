86989716e7b89676eb468820b00f61793d36c729
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2023-04-21 16:39:01 -0300

    Add canvas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Canvas from "./canvas/index"
+import CanvasModel from "./canvas/index"
 import Customizer from "./pages/Customizer"
 import Home from "./pages/Home"
 
 function App() {
   return (
     <main className="app transition-all ease-in">
-      <Canvas />
+      <CanvasModel />
       <Customizer />
       <Home />
     </main>

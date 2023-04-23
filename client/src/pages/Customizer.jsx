22ba4a7c1fe12507cbf93970e82f4612570130b6
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2023-04-23 17:31:22 -0300

    Add file picker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -23,10 +23,10 @@ const Customizer = () => {
   const [prompt, setPrompt] = useState("")
   const [generatingImg, setGeneratingImg] = useState(false)
 
-  const [activeEditorTab, setEditorTab] = useState("")
+  const [activeEditorTab, setActiveEditorTab] = useState("")
   const [activeFilterTab, setActiveFilterTab] = useState({
-    logoShirte: true,
-    stylishShirt: false,
+    logoShirt: true,
+    styleshShirt: false,
   })
 
   const generateTabContent = () => {
@@ -34,13 +34,54 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />
       case "filepicker":
-        return <FilePicker />
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />
       case "aipicker":
         return <AIPicker />
 
       default:
         return null
     }
+  }
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type]
+
+    state[decalType.stateProperty] = result
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName]
+        break
+
+      case "styleshShirt":
+        state.isFullTexture = !activeFilterTab[tabName]
+        break
+
+      default:
+        state.isLogoTexture = true
+        state.isFullTexture = false
+        break
+    }
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      }
+    })
+  }
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result)
+      setActiveEditorTab("")
+    })
   }
 
   return (
@@ -59,7 +100,7 @@ const Customizer = () => {
                     key={tab.name}
                     tab={tab}
                     handleClick={() => {
-                      setEditorTab(tab.name)
+                      setActiveEditorTab(tab.name)
                     }}
                   />
                 ))}
@@ -90,8 +131,10 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => {
+                  handleActiveFilterTab(tab.name)
+                }}
               />
             ))}
           </motion.div>

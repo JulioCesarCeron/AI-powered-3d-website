9eda0f3926d445f8026280c42d184e0cb2eea8b1
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2023-06-08 18:19:41 -0300

    upgrade libs

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/AI-powered-3d-website//client/dist/",
+  base: "/AI-powered-3d-website/client/dist/",
   plugins: [react()],
 })
